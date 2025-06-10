4a80f3a3ef97035cbde0bf6d74eb4585652688d5
Zona Airdrop <80012026+ZonaAirdrop@users.noreply.github.com>
2025-06-10 14:39:05 +0700

    Update gotchipus.js

--- gotchipus.js
+++ gotchipus.js
@@ -22,7 +22,7 @@ const logger = {
   banner: () => {
     console.log(`${colors.cyan}${colors.bold}`);
     console.log(`---------------------------------------------`);
-    console.log(`  Gotchipus Mint Auto Bot - YetiDAO  `);
+    console.log(`  Gotchipus Auto Mint Bot - by zonaairdrop `);
     console.log(`---------------------------------------------${colors.reset}`);
     console.log();
   }
