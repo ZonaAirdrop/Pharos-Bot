eb572270e913bacc8265af70c6147b026ef4e755
Zona Airdrop <80012026+ZonaAirdrop@users.noreply.github.com>
2025-06-27 17:21:41 +0700

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -127,7 +127,7 @@ class PharosTestnet:
 
     def welcome(self):
         print(Fore.LIGHTGREEN_EX + Style.BRIGHT + "\n" + "═" * 60)
-        print(Fore.GREEN + Style.BRIGHT + "    Pharos BOT Tesnet")
+        print(Fore.GREEN + Style.BRIGHT + "    ⚡Pharos X Zenith Tesnet⚡")
         print(Fore.CYAN + Style.BRIGHT + "    ────────────────────────────────")
         print(Fore.YELLOW + Style.BRIGHT + "    Team zonaairdrop")
         print(Fore.CYAN + Style.BRIGHT + "    ────────────────────────────────")
