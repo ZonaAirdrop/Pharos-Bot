a3e2d90cebc91a278864a7279b43d802b4da3e88
Zona Airdrop <80012026+ZonaAirdrop@users.noreply.github.com>
2025-06-28 14:49:07 +0700

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -116,11 +116,13 @@ class PharosTestnet:
         print(Fore.LIGHTGREEN_EX + Style.BRIGHT + "\n" + "═" * 60)
         print(Fore.GREEN + Style.BRIGHT + "    ⚡ Pharos X Faroswap Tesnet ⚡")
         print(Fore.CYAN + Style.BRIGHT + "    ────────────────────────────────")
-        print(Fore.YELLOW + Style.BRIGHT + "    Team zonaairdrop")
+        print(Fore.YELLOW + Style.BRIGHT + "    Team : Zonaairdrop")
         print(Fore.CYAN + Style.BRIGHT + "    ────────────────────────────────")
-        print(Fore.MAGENTA + Style.BRIGHT + "     Powered by Zonaairdrop")
+        print(Fore.RED + Style.BRIGHT + "   Channel telegram : @ZonaAirdr0p")
+        print(Fore.CYAN + Style.BRIGHT + "    ────────────────────────────────")
+        print(Fore.MAGENTA + Style.BRIGHT + "   Powered by Zonaairdrop")
         print(Fore.LIGHTGREEN_EX + Style.BRIGHT + "═" * 60 + "\n")
-
+        
     def format_seconds(self, seconds):
         hours, remainder = divmod(seconds, 3600)
         minutes, seconds = divmod(remainder, 60)
