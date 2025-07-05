02c88228681011456e49c022768652089d383488
Zona Airdrop <80012026+ZonaAirdrop@users.noreply.github.com>
2025-07-05 22:36:51 +0700

    Update bot6.py

--- bot6.py
+++ bot6.py
@@ -55,13 +55,13 @@ class PharosTestnet:
         self.SWAP_CONTRACT_ABI = [
             {
                 "inputs": [
-                    { "internalType": "uint256", "name": "collectionAndSelfcalls", "type": "uint256" },
-                    { "internalType": "bytes[]", "name": "data", "type": "bytes[]" }
+                    {"internalType": "uint256", "name": "deadline", "type": "uint256"},
+                    {"internalType": "bytes[]", "name": "data", "type": "bytes[]"}
                 ],
                 "name": "multicall",
                 "outputs": [],
-                "stateMutability": "nonpayable",
-                "type": "function",
+                "stateMutability": "payable",
+                "type": "function"
             }
         ]
         self.ADD_LP_CONTRACT_ABI = [
@@ -84,18 +84,18 @@ class PharosTestnet:
                         "internalType": "struct INonfungiblePositionManager.MintParams",
                         "name": "params",
                         "type": "tuple",
-                    },
+                    }
                 ],
                 "name": "mint",
                 "outputs": [
                     { "internalType": "uint256", "name": "tokenId", "type": "uint256" },
                     { "internalType": "uint128", "name": "liquidity", "type": "uint128" },
                     { "internalType": "uint256", "name": "amount0", "type": "uint256" },
-                    { "internalType": "uint256", "name": "amount1", "type": "uint256" },
+                    { "internalType": "uint256", "name": "amount1", "type": "uint256" }
                 ],
                 "stateMutability": "payable",
-                "type": "function",
-            },
+                "type": "function"
+            }
         ]
         self.ref_code = "8G8MJ3zGB7tJgP"
         self.proxies = []
