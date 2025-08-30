4ba42f53977ed2c99559361d03f378d24145ebf3
Zona Airdrop <80012026+ZonaAirdrop@users.noreply.github.com>
2025-08-31 01:31:58 +0700

    Update bot10.py

--- bot10.py
+++ bot10.py
@@ -8,11 +8,9 @@ from colorama import *
 import asyncio, random, json, re, os
 from dotenv import load_dotenv
 
-# Initialize colorama with autoreset
 init(autoreset=True)
 load_dotenv()
 
-# === Terminal Color Setup ===
 class Colors:
     RESET = Style.RESET_ALL
     BOLD = Style.BRIGHT
@@ -60,7 +58,7 @@ async def display_welcome_screen():
     now = datetime.now()
     print(f"{Colors.BRIGHT_GREEN}{Colors.BOLD}")
     print("  ╔══════════════════════════════════════╗")
-    print("  ║              B O T                   ║")
+    print("  ║             Pharos R2 B O T                   ║")
     print("  ║                                      ║")
     print(f"  ║     {Colors.YELLOW}{now.strftime('%H:%M:%S %d.%m.%Y')}{Colors.BRIGHT_GREEN}           ║")
     print("  ║                                      ║")
